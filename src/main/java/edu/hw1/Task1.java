66eee37d4d61b06f0d598bedbb0525b823544172
Ayazkins <10a-yahinayaz@mail.ru>
2023-10-13 01:19:42 +0300

    succesfull build

--- src/main/java/edu/hw1/Task1.java
+++ src/main/java/edu/hw1/Task1.java
@@ -6,6 +6,7 @@ public final class Task1 {
 
     private static final int SECONDS_IN_MINUTES = 60;
 
+    @SuppressWarnings("ReturnCount")
     public static int minutesToSeconds(String time) {
         if (time == null) {
             return -1;
@@ -22,9 +23,12 @@ public final class Task1 {
         } catch (NumberFormatException exception) {
             return -1;
         }
+        int answer;
         if (seconds >= SECONDS_IN_MINUTES || seconds < 0 || minutes < 0) {
-            return -1;
+            answer = -1;
+        } else {
+            answer = minutes * SECONDS_IN_MINUTES + seconds;
         }
-        return minutes * SECONDS_IN_MINUTES + seconds;
+        return answer;
     }
 }
--- src/main/java/edu/hw1/Task6.java
+++ src/main/java/edu/hw1/Task6.java
@@ -13,10 +13,12 @@ public final class Task6 {
     private static final int FOURTH_DIGIT = 1000;
 
 
+
     private Task6() {
 
     }
 
+    @SuppressWarnings("MagicNumber")
     public static int countK(int inputNumber) {
         if (inputNumber == K_NUMBER) {
             return 0;
@@ -28,11 +30,11 @@ public final class Task6 {
         int[] array = new int[AMOUNT_OF_DIGITS];
         for (int i = 0; i < AMOUNT_OF_DIGITS; ++i) {
             array[i] = number % DIVISION_TEN;
-            number /= 10;
+            number /= DIVISION_TEN;
         }
         Arrays.sort(array);
-        int minNumber = array[0] * FOURTH_DIGIT + array[1] * THIRD_DIGIT + array[2] * 10 + array[3];
-        int maxNumber = array[3] * FOURTH_DIGIT + array[2] * THIRD_DIGIT + array[1] * 10 + array[0];
+        int minNumber = array[0] * FOURTH_DIGIT + array[1] * THIRD_DIGIT + array[2] * DIVISION_TEN + array[3];
+        int maxNumber = array[3] * FOURTH_DIGIT + array[2] * THIRD_DIGIT + array[1] * DIVISION_TEN + array[0];
         if (maxNumber - minNumber == K_NUMBER) {
             return 1;
         } else {

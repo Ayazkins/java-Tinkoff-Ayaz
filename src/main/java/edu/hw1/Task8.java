512ce96e1a50377a72200d6515f1855e7472fa32
Ayazkins <10a-yahinayaz@mail.ru>
2023-10-13 01:28:47 +0300

    debug task8

--- src/main/java/edu/hw1/Task8.java
+++ src/main/java/edu/hw1/Task8.java
@@ -1,5 +1,7 @@
 package edu.hw1;
 
+import java.io.Console;
+
 public final class Task8 {
 
 
@@ -20,7 +22,7 @@ public final class Task8 {
     }
 
     public static boolean check(int pos1, int pos2) {
-        return pos1 >= 0 & pos1 < BOARD_SIZE & pos2 >= 0 & pos2 < BOARD_SIZE;
+        return pos1 >= 0 && pos1 < BOARD_SIZE && pos2 >= 0 && pos2 < BOARD_SIZE;
     }
 
     public static boolean knightBoardCapture(int[][] board) {
@@ -35,7 +37,7 @@ public final class Task8 {
                 if (board[i][j] == HORSE) {
                     for (var t : HELP) {
                         int pos1 = i + t[0];
-                        int pos2 = i + t[1];
+                        int pos2 = j + t[1];
                         if (check(pos1, pos2) && board[pos1][pos2] == HORSE) {
                             return false;
                         }

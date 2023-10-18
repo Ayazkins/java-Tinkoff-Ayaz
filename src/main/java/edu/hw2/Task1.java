4979fca2cc0921fe7cea839296e13aa2502cdbe0
Ayazkins <10a-yahinayaz@mail.ru>
2023-10-18 18:53:55 +0300

    constructors for records

--- src/main/java/edu/hw2/Task1.java
+++ src/main/java/edu/hw2/Task1.java
@@ -5,14 +5,18 @@ public class Task1 {
     public sealed interface Expr {
         double evaluate();
 
-        public record Constant(double value) implements Expr {
+        record Constant(double value) implements Expr {
             @Override
             public double evaluate() {
                 return value;
             }
         }
 
-        public record Negate(Expr value) implements Expr {
+        record Negate(Expr value) implements Expr {
+
+            public Negate(double value) {
+                this(new Constant(value));
+            }
 
             @Override
             public double evaluate() {
@@ -20,14 +24,39 @@ public class Task1 {
             }
         }
 
-        public record Exponent(Expr value, Expr value2) implements Expr {
+        record Exponent(Expr value, Expr value2) implements Expr {
+
+            Exponent(double value, double value2) {
+                this(new Constant(value), new Constant(value2));
+            }
+
+            Exponent(Expr value, double value2) {
+                this(value, new Constant(value2));
+            }
+
+            Exponent(double value, Expr value2) {
+                this(new Constant(value), value2);
+            }
+
             @Override
             public double evaluate() {
                 return Math.pow(value.evaluate(), value2.evaluate());
             }
         }
 
-        public record Addition(Expr value, Expr value2) implements Expr {
+        record Addition(Expr value, Expr value2) implements Expr {
+
+            Addition(double value, double value2) {
+                this(new Constant(value), new Constant(value2));
+            }
+
+            Addition(Expr value, double value2) {
+                this(value, new Constant(value2));
+            }
+
+            Addition(double value, Expr value2) {
+                this(new Constant(value), value2);
+            }
 
             @Override
             public double evaluate() {
@@ -35,7 +64,20 @@ public class Task1 {
             }
         }
 
-        public record Multiplication(Expr value, Expr value2) implements Expr {
+        record Multiplication(Expr value, Expr value2) implements Expr {
+
+            Multiplication(double value, double value2) {
+                this(new Constant(value), new Constant(value2));
+            }
+
+            Multiplication(Expr value, double value2) {
+                this(value, new Constant(value2));
+            }
+
+            Multiplication(double value, Expr value2) {
+                this(new Constant(value), value2);
+            }
+
             @Override
             public double evaluate() {
                 return value.evaluate() * value2.evaluate();

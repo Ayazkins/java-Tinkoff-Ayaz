465d09d59deac45d8ecc080f307bfc0ca4d2fa3a
Ayazkins <10a-yahinayaz@mail.ru>
2023-10-20 12:12:23 +0300

    it works

--- src/main/java/edu/hw1/Main.java
+++ src/main/java/edu/hw1/Main.java
@@ -1,7 +1,5 @@
 package edu.hw1;
 
-import edu.project1.DefaultDictionary;
-import edu.project1.Game;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -14,20 +12,6 @@ public final class Main {
     }
 
     public static void main(String[] args) {
-        var dict = new DefaultDictionary();
-        dict.addWord("coffee");
-        var game = new Game(dict);
-        game.launch();
-//        // Press Alt+Enter with your caret at the highlighted text to see how
-//        // IntelliJ IDEA suggests fixing it.
-//        LOGGER.info("Hello and welcome!");
-//
-//        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-//        for (int i = 0; i <= 2; i++) {
-//
-//            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-//            // for you, but you can always add more by pressing Ctrl+F8.
-//            LOGGER.info("i = {}", i);
-//        }
+
     }
 }

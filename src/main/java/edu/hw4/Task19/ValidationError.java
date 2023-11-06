239a62c306d06b23099d3f1ab03231fa3a11f50a
Ayazkins <10a-yahinayaz@mail.ru>
2023-11-06 23:37:38 +0300

    fix everythin

--- src/main/java/edu/hw4/Task19/ValidationError.java
+++
@@ -1,9 +0,0 @@
-package edu.hw4.Task19;
-
-public record ValidationError(String fieldName, String message) {
-
-    @Override
-    public String toString() {
-        return fieldName + ": " + message;
-    }
-}

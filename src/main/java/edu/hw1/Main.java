a58869ddfd1ecc5e63e777c3595ea07123db3cd8
Ayazkins <10a-yahinayaz@mail.ru>
2023-11-06 10:10:53 +0300

    little checkstyle fixed

--- src/main/java/edu/hw1/Main.java
+++ src/main/java/edu/hw1/Main.java
@@ -12,14 +12,14 @@ public final class Main {
     }
 
     public static void main(String[] args) {
-        BacktrackingGenerator backtrackingGenerator = new BacktrackingGenerator();
-        Maze maze = backtrackingGenerator.generate(200, 200);
-        Coordinate start = new Coordinate(1, 1);
-        Coordinate end = new Coordinate(199, 199);
-        BackTrackingSolver backTrackingSolver = new BackTrackingSolver();
-        backTrackingSolver.solve(maze, start, end);
-        JComponentRender jComponentRender = new JComponentRender();
-        jComponentRender.renderPath(maze, backTrackingSolver.coordinates, backTrackingSolver.cells);
+//        BacktrackingGenerator backtrackingGenerator = new BacktrackingGenerator();
+//        Maze maze = backtrackingGenerator.generate(200, 200);
+//        Coordinate start = new Coordinate(1, 1);
+//        Coordinate end = new Coordinate(199, 199);
+//        BackTrackingSolver backTrackingSolver = new BackTrackingSolver();
+//        backTrackingSolver.solve(maze, start, end);
+//        JComponentRender jComponentRender = new JComponentRender();
+//        jComponentRender.renderPath(maze, backTrackingSolver.coordinates, backTrackingSolver.cells);
 
     }
 }

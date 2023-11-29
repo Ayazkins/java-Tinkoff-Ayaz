d9eb00fbca770aef836cb8343257a2638efd0b82
Ayazkins <10a-yahinayaz@mail.ru>
2023-11-29 23:24:08 +0300

    fixes

--- src/main/java/edu/hw7/Task3/PersonDatabaseLock.java
+++ src/main/java/edu/hw7/Task3/PersonDatabaseLock.java
@@ -3,6 +3,7 @@ package edu.hw7.Task3;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -10,21 +11,14 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
-public class PersonDatabaseLock implements PersonDatabase {
-    private final Map<Integer, Person> personMap = new HashMap<>();
-    private final Map<String, Set<Integer>> nameIndex = new HashMap<>();
-    private final Map<String, Set<Integer>> addressIndex = new HashMap<>();
-    private final Map<String, Set<Integer>> phoneIndex = new HashMap<>();
+public class PersonDatabaseLock extends PersonDatabaseMethods implements PersonDatabase {
     private final ReadWriteLock lock = new ReentrantReadWriteLock();
 
     @Override
     public void add(Person person) {
         lock.writeLock().lock();
         try {
-            personMap.put(person.id(), person);
-            nameIndex.computeIfAbsent(person.name(), k -> ConcurrentHashMap.newKeySet()).add(person.id());
-            addressIndex.computeIfAbsent(person.address(), k -> ConcurrentHashMap.newKeySet()).add(person.id());
-            phoneIndex.computeIfAbsent(person.phoneNumber(), k -> ConcurrentHashMap.newKeySet()).add(person.id());
+            addToBase(person);
         } finally {
             lock.writeLock().unlock();
         }
@@ -34,12 +28,7 @@ public class PersonDatabaseLock implements PersonDatabase {
     public void delete(int id) {
         lock.writeLock().lock();
         try {
-            Person person = personMap.remove(id);
-            if (person != null) {
-                nameIndex.getOrDefault(person.name(), Collections.emptySet()).remove(id);
-                addressIndex.getOrDefault(person.address(), Collections.emptySet()).remove(id);
-                phoneIndex.getOrDefault(person.phoneNumber(), Collections.emptySet()).remove(id);
-            }
+            deleteFromBase(id);
         } finally {
             lock.writeLock().unlock();
         }
@@ -49,8 +38,7 @@ public class PersonDatabaseLock implements PersonDatabase {
     public List<Person> findByName(String name) {
         lock.readLock().lock();
         try {
-            Set<Integer> ids = nameIndex.getOrDefault(name, Collections.emptySet());
-            return findPersonsByIds(ids);
+            return findByNameAbstract(name);
         } finally {
             lock.readLock().unlock();
         }
@@ -60,8 +48,7 @@ public class PersonDatabaseLock implements PersonDatabase {
     public List<Person> findByAddress(String address) {
         lock.readLock().lock();
         try {
-            Set<Integer> ids = addressIndex.getOrDefault(address, Collections.emptySet());
-            return findPersonsByIds(ids);
+            return findByAddressAbstract(address);
         } finally {
             lock.readLock().unlock();
         }
@@ -71,21 +58,9 @@ public class PersonDatabaseLock implements PersonDatabase {
     public List<Person> findByPhone(String phone) {
         lock.readLock().lock();
         try {
-            Set<Integer> ids = phoneIndex.getOrDefault(phone, Collections.emptySet());
-            return findPersonsByIds(ids);
+            return findByPhoneAbstract(phone);
         } finally {
             lock.readLock().unlock();
         }
-    }
-
-    private List<Person> findPersonsByIds(Set<Integer> ids) {
-        List<Person> result = new ArrayList<>();
-        for (int id : ids) {
-            Person person = personMap.get(id);
-            if (person != null) {
-                result.add(person);
-            }
-        }
-        return result;
     }
 }
--- src/main/java/edu/hw7/Task3/PersonDatabaseService.java
+++ src/main/java/edu/hw7/Task3/PersonDatabaseService.java
@@ -3,63 +3,35 @@ package edu.hw7.Task3;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class PersonDatabaseService implements PersonDatabase {
-    private final Map<Integer, Person> personMap = new HashMap<>();
-    private final Map<String, Set<Integer>> nameIndex = new HashMap<>();
-    private final Map<String, Set<Integer>> addressIndex = new HashMap<>();
-    private final Map<String, Set<Integer>> phoneIndex = new HashMap<>();
-
+public class PersonDatabaseService extends PersonDatabaseMethods implements PersonDatabase {
     @Override
     public synchronized void add(Person person) {
-        personMap.put(person.id(), person);
-        nameIndex.computeIfAbsent(person.name(), k -> ConcurrentHashMap.newKeySet()).add(person.id());
-        addressIndex.computeIfAbsent(person.address(), k -> ConcurrentHashMap.newKeySet()).add(person.id());
-        phoneIndex.computeIfAbsent(person.phoneNumber(), k -> ConcurrentHashMap.newKeySet()).add(person.id());
+        addToBase(person);
     }
 
     @Override
     public synchronized void delete(int id) {
-        Person person = personMap.remove(id);
-        if (person != null) {
-            synchronized (this) {
-                nameIndex.getOrDefault(person.name(), Collections.emptySet()).remove(id);
-                addressIndex.getOrDefault(person.address(), Collections.emptySet()).remove(id);
-                phoneIndex.getOrDefault(person.phoneNumber(), Collections.emptySet()).remove(id);
-            }
-        }
+        deleteFromBase(id);
     }
 
     @Override
     public synchronized List<Person> findByName(String name) {
-        Set<Integer> ids = nameIndex.getOrDefault(name, Collections.emptySet());
-        return findPersonsByIds(ids);
+        return findByNameAbstract(name);
     }
 
     @Override
     public synchronized List<Person> findByAddress(String address) {
-        Set<Integer> ids = addressIndex.getOrDefault(address, Collections.emptySet());
-        return findPersonsByIds(ids);
+        return findByAddressAbstract(address);
     }
 
     @Override
     public synchronized List<Person> findByPhone(String phone) {
-        Set<Integer> ids = phoneIndex.getOrDefault(phone, Collections.emptySet());
-        return findPersonsByIds(ids);
-    }
-
-    private synchronized List<Person> findPersonsByIds(Set<Integer> ids) {
-        List<Person> result = new ArrayList<>();
-        for (int id : ids) {
-            Person person = personMap.get(id);
-            if (person != null) {
-                result.add(person);
-            }
-        }
-        return result;
+        return findByPhoneAbstract(phone);
     }
 }
